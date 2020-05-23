964bd2d632e88915ed5f24d73c9fbf68a2db7584
小桔子 <63684402+xjz3103@users.noreply.github.com>
2020-05-23 15:11:16 +0800

    2

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`123 ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">

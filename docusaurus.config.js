4990882acb12c22b30f52ddeff3d7e07dbfce43f
xjz3103 <1140535362@qq.com>
2020-06-26 23:09:24 +0800

    2020年6月26日23:09

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,100 +1,100 @@
 module.exports = {
-  title: 'JACK小桔子',
-  tagline: '一个分享软件应用/插件/实用技巧的网站',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: 'img/logo.jpg',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "JACK小桔子",
+  tagline: "一个分享软件应用/插件/实用技巧的网站",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  favicon: "img/logo.jpg",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'JACK小桔子',
+      title: "JACK小桔子",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/favicon.ico',
+        alt: "My Site Logo",
+        src: "img/favicon.ico",
       },
       links: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: '文档',
-          position: 'right',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "文档",
+          position: "right",
         },
-        {to: 'blog', label: '博客', position: 'right'},
+        { to: "blog", label: "博客", position: "right" },
         {
-          href: 'https://github.com/xjz3103',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/xjz3103",
+          label: "GitHub",
+          position: "right",
         },
         {
-          href: 'https://space.bilibili.com/481261311',
-          label: 'bilibili',
-          position: 'right',
+          href: "https://space.bilibili.com/481261311",
+          label: "bilibili",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: '电话',
+          title: "电话",
           items: [
             {
-              label: '13413293886',
+              label: "13413293886",
             },
           ],
         },
         {
-          title: '电子邮箱',
+          title: "电子邮箱",
           items: [
             {
-              label: '1140535362@qq.com',
+              label: "1140535362@qq.com",
             },
             {
-              label: 'xjz3103@outlook.com',
+              label: "xjz3103@outlook.com",
             },
             {
-              label: 'xjz3103@gmail.com',
+              label: "xjz3103@gmail.com",
             },
           ],
         },
         {
-          title: '联系我',
+          title: "联系我",
           items: [
             {
-              label: 'bilibili',
-              href: 'https://space.bilibili.com/481261311',
+              label: "bilibili",
+              href: "https://space.bilibili.com/481261311",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/xjz3103',
+              label: "GitHub",
+              href: "https://github.com/xjz3103",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} JACK小桔子`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'docx1',
-          sidebarPath: require.resolve('./sidebars.js'),
+          homePageId: "docx1",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
